4e568431d63c1a2c985e87641bad8c3f61ac4912
Lukesh Patil <lukeshpatil05@gmail.com>
2024-06-09 01:11:32 +0530

    Implemented collision detection

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ ctx.fillRect(0,0,canvas.width,canvas.height);
 class Particle{
     constructor(effect){
         this.effect = effect;
-        this.radius = Math.random()*10 + 20;
+        this.radius = Math.random()*15 + 15;
         this.x = this.radius + Math.random()*(this.effect.width-2*this.radius);
         this.y = this.radius + Math.random()*(this.effect.height-2*this.radius);
         this.speedX = Math.random()*10-5;
@@ -49,7 +49,7 @@ class Effect{
         this.width = canvas.width;
         this.height = canvas.height;
 
-        this.numberOfParticles = 15;
+        this.numberOfParticles = 11;
         this.particleArray = [];
 
         this.createParticles();
@@ -60,6 +60,51 @@ class Effect{
             this.particleArray.push(new Particle(this));
         }
     }
+
+    handleCollision() {
+        for (let i = 0; i < this.numberOfParticles; i++) {
+            for (let j = i + 1; j < this.numberOfParticles; j++) {
+                const particle1 = this.particleArray[i];
+                const particle2 = this.particleArray[j];
+    
+                const dx = particle2.x - particle1.x;
+                const dy = particle2.y - particle1.y;
+                const distance = Math.hypot(dx, dy);
+                const combinedRadius = particle1.radius + particle2.radius;
+    
+                if (distance < combinedRadius) {
+                    // Collision detected
+                    const angle = Math.atan2(dy, dx);
+                    const sin = Math.sin(angle);
+                    const cos = Math.cos(angle);
+    
+                    // Rotate particle velocities
+                    const velX1 = cos * particle1.speedX + sin * particle1.speedY;
+                    const velY1 = cos * particle1.speedY - sin * particle1.speedX;
+                    const velX2 = cos * particle2.speedX + sin * particle2.speedY;
+                    const velY2 = cos * particle2.speedY - sin * particle2.speedX;
+    
+                    // Exchange velocities
+                    const tempVelX = velX1;
+                    particle1.speedX = cos * velX2 - sin * velY1;
+                    particle1.speedY = sin * velX2 + cos * velY1;
+                    particle2.speedX = cos * tempVelX - sin * velY2;
+                    particle2.speedY = sin * tempVelX + cos * velY2;
+    
+                    // Separate the particles so they don't overlap
+                    const overlap = combinedRadius - distance;
+                    const separationX = cos * overlap / 2;
+                    const separationY = sin * overlap / 2;
+    
+                    particle1.x -= separationX;
+                    particle1.y -= separationY;
+                    particle2.x += separationX;
+                    particle2.y += separationY;
+                }
+            }
+        }
+    }
+    
 
     connectParticles(context){
         const maxDistance = 300;
@@ -102,6 +147,8 @@ class Effect{
         this.particleArray.forEach(function(particle){
             particle.update();
         });
+
+        this.handleCollision();
 
         this.connectParticles(context);
 
